98629322e7ba5ab18ee96d0c75561e968376d8f6
HAXMINATOR <thor_e_jensen@hotmail.com>
2019-05-15 13:18:03 +0200

    Added classes from the other project

---
+++ Warlock The Soulbinder/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warlock_The_Soulbinder
+{
+    class Character : GameObject
+    {
+        protected float movementSpeed;
+        private string name;
+
+        public Character(int index)
+        {
+
+        }
+
+        protected string Name { get => name; }
+    }
+}
